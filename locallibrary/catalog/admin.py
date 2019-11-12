97c00235b0bfeb214bcde83289f2752c3f15f4ff
Ossi Lehto <45407456+nosh1337@users.noreply.github.com>
2019-11-12 10:50:50 +0200

    Mozilla Django tutorial
    https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Tutorial_local_library_website

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance, Language
+# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
